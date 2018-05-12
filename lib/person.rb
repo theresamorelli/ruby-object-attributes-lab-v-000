755379f49179da559b84717fc6a389d71a911ae6
Theresa Morelli <theresa.l.morelli@gmail.com>
2018-05-12 16:43:04 +0000

    automatically backed up by learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,5 +1,19 @@
 class Person
 
-  attr_accessor :name, :job
+  def name
+    @name
+  end
+
+  def job
+    @job
+  end
+
+  def name=(new_name)
+    @name = new_name
+  end
+
+  def job=(new_job)
+    @job = new_job
+  end
 
 end
